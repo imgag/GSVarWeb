ba67da08d860944d05afb26c58a8d9fe79e3ae99
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-02-27 12:50:59 +0100

    add download controller

--- api/swagger_server/__main__.py
+++ api/swagger_server/__main__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import os
+
 import connexion
 
 from swagger_server import encoder
@@ -9,6 +11,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'ngs-bits'})
+    app.app.config['UPLOAD_FOLDER'] = os.path.abspath(os.getenv('NGS_BITS_DATA', os.getcwd()))
     app.run(port=8080)
 
 
--- api/swagger_server/controllers/default_controller.py
+++ api/swagger_server/controllers/default_controller.py
@@ -1,3 +1,6 @@
+import os
+
+from flask import current_app, send_from_directory, abort
 import connexion
 import six
 
@@ -14,4 +17,8 @@ def download_file_path_get(filePath):  # noqa: E501
 
     :rtype: file
     """
-    return 'do some magic!'
+    absFilePath = os.path.join(current_app.config['UPLOAD_FOLDER'], filePath)
+    if os.path.isfile(absFilePath): # this makes sure the upload folder is not escaped
+        return send_from_directory(directory=current_app.config['UPLOAD_FOLDER'], filename=filePath)
+    else:
+        abort(404)
