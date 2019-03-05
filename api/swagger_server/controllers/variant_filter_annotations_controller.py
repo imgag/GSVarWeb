c72359e6a97a5684e2679de7b22358da39e353fb
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-03-05 10:52:36 +0100

    Fix bug with closing file handle in filter request.

--- api/swagger_server/controllers/variant_filter_annotations_controller.py
+++ api/swagger_server/controllers/variant_filter_annotations_controller.py
@@ -29,21 +29,20 @@ def variant_filter_annotations_post(body=None):  # noqa: E501
     if os.path.isfile(absInPath) and not os.path.isfile(absOutPath):
         lines = convert_dict_to_lines(body.filter)
         tmpPath = os.path.join(tempfile.gettempdir(), tempfile._get_candidate_names().characters)
-        print(tmpPath)
-        with open(tmpPath, "w") as tmpFile:
+        with open(tmpPath, "w") as tmpFile: # write filters file
             tmpFile.write(lines)
 
-            # Run VariantFilterAnnotations
-            binFolder = os.path.abspath(os.getenv('NGS_BITS_BIN', os.getcwd()))
-            command = "./VariantFilterAnnotations -in {} -out {} -filters {}".format(absInPath, absOutPath, tmpFile.name)
-            fullCommand = "cd {} && {}".format(binFolder, command)
-            print(fullCommand)
+        # Run VariantFilterAnnotations
+        binFolder = os.path.abspath(os.getenv('NGS_BITS_BIN', os.getcwd()))
+        command = "./VariantFilterAnnotations -in {} -out {} -filters {}".format(absInPath, absOutPath, tmpFile.name)
+        fullCommand = "cd {} && {}".format(binFolder, command)
+        current_app.logger.info("Running {}".format(fullCommand))
 
-            status = os.system(fullCommand)
-            if status == 0:
-                return "successful"
-            else:
-                abort(400) # TODO: Make this more detailed
+        status = os.system(fullCommand)
+        if status == 0:
+            return "successful"
+        else:
+            abort(400) # TODO: Make this more detailed
 
         abort(400) # message="Could not create the filter file"
     else:
