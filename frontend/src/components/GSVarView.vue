c82d121a15920bb78c7618f154a98f0bdcfe3a95
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-03-06 11:51:42 +0100

    port color code and do some refactoring

--- frontend/src/components/GSVarView.vue
+++ frontend/src/components/GSVarView.vue
@@ -20,7 +20,8 @@
 <script>
 import { produceHeaders } from "@/utils";
 
-/// NOTE: Ported from https://github.com/imgag/ngs-bits/blob/master/src/GSvar/VariantTable.cpp#L114
+/// Ported from https://github.com/imgag/ngs-bits/blob/master/src/GSvar/VariantTable.cpp#L114
+/// Since the headers are sorted this array also needs to be sorted by index!
 const colorHeaders = ["gene", "coding_and_splicing", "ClinVar", "HGMD", "NGSD_hom", "NGSD_het", "classification", "validation", "comment"]
 
 export default {
@@ -45,23 +46,49 @@ export default {
         colorIndexes () {
             let headers = this.headers.map((header) => header.value)
             return headers.filter((header) => colorHeaders.includes(header)).map((header) => headers.indexOf(header))
+        },
+        colorMap () {
+            return this.colorIndexes.reduce((result, item, index) => {
+                result[colorHeaders[index]] = item
+                return result
+            }, {})
         }
     },
     methods: {
         getColor(item, index) {
             let color = ''
 
-            if (index === this.colorIndexes[1] && item.includes(':HIGH:')) {
-                color = 'red'
-            } else if (index === this.colorIndexes[6] && (item === "3" || item === "M")) {
-                color = 'orange'
-            } else if (index === this.colorIndexes[6] && (item === "4" || item === "5")) {
-                color = 'red'
-            } else if (index === this.colorIndexes[2] && item.includes("pathogenic")) {
-                color = 'red'
-            } else if (index === this.colorIndexes[3] && item.includes("CLASS=DM")) {
+            // warning
+            if (index === this.colorMap["coding_and_splicing"] && item.includes(':HIGH:')) {
                 color = 'red'
             }
+
+            if (index === this.colorMap["classification"] && (item === "3" || item === "M")) {
+                color = 'orange'
+            } else if (index === this.colorMap["classification"] && (item === "4" || item === "5")) {
+                color = 'red'
+            } else if (index === this.colorMap["classification"] && item.includes("pathogenic")) {
+                color = 'red'
+            } else if (index === this.colorMap["classification"] && item.includes("CLASS=DM")) {
+                color = 'red'
+            }
+
+            if (index === this.colorMap["classification"] && (item === "0" || item === "1" || item === "2")) {   // non-pathogenic
+                color = 'green'
+            }
+
+            // highlighted
+            if (index === this.colorMap["validation"] && item.includes("TP")) {
+                color = 'yellow'
+            } else if (index === this.colorMap["comment"] && item !== "") {
+                color = 'yellow'
+            } else if (index === this.colorMap["NGSD_hom"] && item === "0") {
+                color = 'yellow'
+            } else if (index === this.colorMap["NGSD_het"] && item === "1") {
+                color = 'yellow'
+            } else if (index === this.colorMap["ClinVar"] && item.includes("confirmed")) {
+                color = 'yellow'
+            } //else if (index === this.colorMap["genes"]) /// TODO: Deal with imprinting genes
 
             return color
         }
