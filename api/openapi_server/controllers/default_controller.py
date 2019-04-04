7eafd83decd9603520835977aabe8ec663a46bf0
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-04-04 16:46:25 +0200

    add tag for megSAP

--- api/openapi_server/controllers/default_controller.py
+++ api/openapi_server/controllers/default_controller.py
@@ -42,41 +42,6 @@ def count_file_path_get(filePath):  # noqa: E501
     else:
         abort(404)
 
-def vcf2gsvar_file_path_get(filePath):  # noqa: E501
-    """vcf2gsvar_file_path_get
-
-    Converts a VCF file to a GSvar # noqa: E501
-
-    :param file_path: The GSvar file to convert
-    :type file_path: str
-
-    :rtype: None
-    """
-
-    absFilePath = os.path.join(current_app.config['UPLOAD_FOLDER'], filePath)
-    if os.path.isfile(absFilePath):
-        absGSvarPath = absFilePath.replace(".vcf", ".GSvar")
-        if not os.path.isfile(absGSvarPath):
-            binFolder = os.path.abspath(os.getenv('NGS_BITS_BIN', os.getcwd()))
-            command = "./VcfCheck -in {}".format(absFilePath)
-            fullCommand = "cd {} && {}".format(binFolder, command)
-
-            status = os.system(fullCommand)
-            if status != 0:
-                raise BadRequest("Command exited with status while checking the VCF {}".format(status))
-
-            megSAP = os.path.abspath(os.getenv('MEGSAP_DIR'))
-            megSAPcommand = "php {}/src/NGS/vcf2gsvar.php -in {} -out {}".format(megSAP, absFilePath, absGSvarPath)
-            status = os.system(megSAPcommand)
-            if status == 0:
-                return os.path.basename(absGSvarPath)
-            else:
-                raise BadRequest("Command failed while producing a GSvar file with status {}".format(status))
-        else:
-            return os.path.basename(absGSvarPath)
-    else:
-        abort(400)
-
 def download_file_path_get(filePath, lines=None):  # noqa: E501
     """download_file_path_get
 
