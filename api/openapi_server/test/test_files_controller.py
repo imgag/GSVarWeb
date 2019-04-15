512c7957f587ddd4e2b51d5e480e2dd0ff68055b
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-04-15 12:23:50 +0200

    Update tags for controllers. Closes #14.

--- api/openapi_server/test/test_files_controller.py
+++ api/openapi_server/test/test_files_controller.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from __future__ import absolute_import
+import unittest
 
 from flask import json
 from six import BytesIO
@@ -8,17 +9,21 @@ from six import BytesIO
 from openapi_server.test import BaseTestCase
 
 
-class TestDefaultController(BaseTestCase):
-    """DefaultController integration test stubs"""
+class TestFilesController(BaseTestCase):
+    """FilesController integration test stubs"""
 
     def test_count_file_path_get(self):
         """Test case for count_file_path_get
 
         
         """
+        headers = { 
+            'Accept': 'application/json',
+        }
         response = self.client.open(
-            '/v1/count/{filePath}'.format(file_path='file_path_example'),
-            method='GET')
+            '/v1/count/{file_path}'.format(file_path='file_path_example'),
+            method='GET',
+            headers=headers)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
@@ -27,23 +32,32 @@ class TestDefaultController(BaseTestCase):
 
         
         """
-        headers = [('lines', 'lines_example')]
+        headers = { 
+            'Accept': 'text/gsvar',
+            'lines': 'lines_example',
+        }
         response = self.client.open(
-            '/v1/download/{filePath}'.format(file_path='file_path_example'),
+            '/v1/download/{file_path}'.format(file_path='file_path_example'),
             method='GET',
             headers=headers)
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
+    @unittest.skip("multipart/form-data not supported by Connexion")
     def test_upload_post(self):
         """Test case for upload_post
 
         
         """
+        headers = { 
+            'Accept': '*/*',
+            'Content-Type': 'multipart/form-data',
+        }
         data = dict(uploaded_file=(BytesIO(b'some file data'), 'file.txt'))
         response = self.client.open(
             '/v1/upload',
             method='POST',
+            headers=headers,
             data=data,
             content_type='multipart/form-data')
         self.assert200(response,
@@ -51,5 +65,4 @@ class TestDefaultController(BaseTestCase):
 
 
 if __name__ == '__main__':
-    import unittest
     unittest.main()
--- api/openapi_server/test/test_ngs_bits_controller.py
+++ api/openapi_server/test/test_ngs_bits_controller.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from __future__ import absolute_import
+import unittest
 
 from flask import json
 from six import BytesIO
@@ -9,18 +10,27 @@ from openapi_server.models.variant_filter_request import VariantFilterRequest  #
 from openapi_server.test import BaseTestCase
 
 
-class TestVariantFilterAnnotationsController(BaseTestCase):
-    """VariantFilterAnnotationsController integration test stubs"""
+class TestNgsBitsController(BaseTestCase):
+    """NgsBitsController integration test stubs"""
 
     def test_variant_filter_annotations_post(self):
         """Test case for variant_filter_annotations_post
 
         
         """
-        variant_filter_request = VariantFilterRequest()
+        variant_filter_request = {
+  "filter" : [ "{}", "{}" ],
+  "in" : "in",
+  "out" : "out"
+}
+        headers = { 
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
         response = self.client.open(
             '/v1/VariantFilterAnnotations',
             method='POST',
+            headers=headers,
             data=json.dumps(variant_filter_request),
             content_type='application/json')
         self.assert200(response,
@@ -28,5 +38,4 @@ class TestVariantFilterAnnotationsController(BaseTestCase):
 
 
 if __name__ == '__main__':
-    import unittest
     unittest.main()
