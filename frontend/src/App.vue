e9b35172c559980f8d777d10801bd71aaca94d58
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-04-18 12:34:54 +0200

    Update site title. Remove favicon. Closes #23.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="inspire">
     <v-toolbar color="indigo" dark fixed app>
-      <v-toolbar-title>ngs-bits</v-toolbar-title>
+      <v-toolbar-title>ngs-remote</v-toolbar-title>
     </v-toolbar>
     <v-content>
       <v-container fluid fill-height>
