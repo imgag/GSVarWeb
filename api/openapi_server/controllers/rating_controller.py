633320e800c322bb25727072237ef0aca1b7fc84
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-07-29 09:23:51 +0200

    Implement annotation view.

--- api/openapi_server/controllers/rating_controller.py
+++ api/openapi_server/controllers/rating_controller.py
@@ -1,12 +1,13 @@
-from flask import request
-import connexion
-import six
+import os
+import io
+
+from flask import current_app, abort, send_file
 from tinydb import Query
 
 from openapi_server import util
 
 
-def annotated_file_path_get(file_path, user=None):  # noqa: E501
+def annotated_file_path_get(filePath, user=None):  # noqa: E501
     """annotated_file_path_get
 
     Retrieves an annotated version of the file. # noqa: E501
@@ -17,10 +18,38 @@ def annotated_file_path_get(file_path, user=None):  # noqa: E501
     :rtype: None
     """
 
-    return 'do some magic!'
+    abs_file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], user, filePath)
+    if os.path.exists(abs_file_path):
+        db = util.get_db()
+        file_query = Query()
+        lines = [] # this is memory inefficient but okay for small files.
+
+        with open(abs_file_path, 'r') as file:
+            for ln, line in enumerate(file):
+                if ln == 4:
+                    lines.append('##DESCRIPTION=gchboc=Annotations by the gchboc task force, seperated using semicolon.\n') # append definition
+                if line.startswith('#') and not line.startswith('##'):
+                    line = line.rstrip() + '\tgchboc\n' # append header
+                else:
+                    partition = line.partition('\t')
+                    chromosome = partition[0]
+                    partition = partition[2].partition('\t')
+                    start = partition[0]
+                    end = partition[2].partition('\t')[0]
+                    ratings = db.search((file_query.name == filePath) & (file_query.chr == chromosome)
+                                        & (file_query.start == start) & (file_query.end == end))
+                    annotation = ';'.join(map(lambda rating: rating['rating'], ratings)) if len(ratings) else '.'
+                    line = line.rstrip() + "\t{}\n".format(annotation)
+                lines.append(line)
+
+        return send_file(io.BytesIO(''.join(lines).encode()),
+                         attachment_filename=filePath,
+                         as_attachment=True)
+    else:
+        abort(404)
 
 
-def rate_put(user=None):  # noqa: E501
+def rate_put(filePath, chr, start, end, rating, user=None):  # noqa: E501
     """rate_file_path_chr_start_end_rating_put
 
     Rate a file. This uses JWT to verify the user. # noqa: E501
@@ -30,24 +59,23 @@ def rate_put(user=None):  # noqa: E501
     :param chr: The chromosome to rate for
     :type chr: str
     :param start: The start position to rate for
-    :type start: 
+    :type start:
     :param end: The end position to annotate for
-    :type end: 
+    :type end:
     :param rating: Which rating this variant should have.
-    :type rating: 
+    :type rating:
 
     :rtype: None
     """
     db = util.get_db()
     file_query = Query()
-    file_path = request.args.get('filePath')
     db.upsert({
-        'name': file_path,
-        'chr': request.args.get('chr'),
-        'start': request.args.get('start'),
-        'end': request.args.get('end'),
-        'rating': request.args.get('rating'),
+        'name': filePath,
+        'chr': chr,
+        'start': start,
+        'end': end,
+        'rating': rating,
         'user': user
-    }, file_query.name == file_path)
+    }, file_query.name == filePath)
 
     return 'successful'
